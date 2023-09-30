17bf4ee0dd9304a78621770b7afa64c01966451d
Noor-Alrai <ameinnoor91@gmail.com>
2023-09-30 13:45:15 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName="Khalid";
+let bestCustomer ="not bob";
+const leastFavoriteCustomer= " John";
+function upperCaseCustomerName(){
+    return customerName.toUpperCase();
+}
+function setBestCustomer(){
+   
+    return bestCustomer;
+}
+function overwriteBestCustomer(){
+bestCustomer= "is bob";
+return bestCustomer
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "not John";
+    return leastFavoriteCustomer;
+}
+console.log(upperCaseCustomerName())
+console.log(setBestCustomer())
+console.log(overwriteBestCustomer())
+console.log(changeLeastFavoriteCustomer()) //error because we change constant variable.
